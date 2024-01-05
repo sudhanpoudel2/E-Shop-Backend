9ca6ed325942c71a187951290956da50f7a8479b
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2024-01-06 00:11:46 +0545

    with error

--- app.js
+++ app.js
@@ -8,41 +8,18 @@ import { dbConnection } from './DB/database.js';
 const app = express();
 const api = process.env.API_URL;
 
-app.use(bodyParser.json()); // Add this line to enable JSON parsing for request bodies
+import productRouter form '../routers/product.router.js'
+
+//middleware
+app.use(bodyParser.json()); 
 app.use(morgan('tiny'));  
 
-import { Product } from './models/product.model.js'; 
+app.use('/api/v1/product*',productRouter)
+
+// import { Product } from './models/product.model.js'; 
 
 // console.log('Received a request to the root endpoint.');
-app.get('/api/v1/product*', async (req, res) => {
-    // Your logic here
-    const productList = await Product.find() 
-    if(!productList){
-        res.status(500).json({success:false})
-    }
-    res.send(productList);
-});
 
-
-app.post('/api/v1/product*', (req, res) => {
-    console.log("Post is working");
-    const product = new Product({
-        name : req.body.name,
-        image : req.body.image,
-        countInStock : req.body.countInStock
-    })
-
-    product.save().then((createProduct =>{
-        res.status(201).json(createProduct)
-    })).catch((e) => {
-        res.status(500).json({
-            error: e,
-            success: false
-        });
-        return; // Add this line to stop further execution
-    });
-    
-});
 
 app.listen(4000 ,()=>{
     // console.log(process.env);
--- models/routers/product.routers.js
+++ models/routers/product.routers.js
@@ -1,1 +1,35 @@
-// import { Router } from "express";
+import express from 'express';
+import router from "express";
+import { Product } from "../product.model.js";
+
+router.get('/api/v1/product*', async (req, res) => {
+    // Your logic here
+    const productList = await Product.find() 
+    if(!productList){
+        res.status(500).json({success:false})
+    }
+    res.send(productList);
+});
+
+
+router.post('/api/v1/product*', (req, res) => {
+    console.log("Post is working");
+    const product = new Product({
+        name : req.body.name,
+        image : req.body.image,
+        countInStock : req.body.countInStock
+    })
+
+    product.save().then((createProduct =>{
+        res.status(201).json(createProduct)
+    })).catch((e) => {
+        res.status(500).json({
+            error: e,
+            success: false
+        });
+        return; // Add this line to stop further execution
+    });
+    
+});
+
+module.exports = router;
