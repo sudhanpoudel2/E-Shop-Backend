ba32ad189a912305eca396738938b5f0126efad3
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2024-01-05 23:29:34 +0545

    product post

--- app.js
+++ app.js
@@ -3,43 +3,45 @@ import 'dotenv/config';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';   
 import { dbConnection } from './DB/database.js'; 
-import { Product } from './Model/product.model.js'; 
 
-   
 
 const app = express();
 const api = process.env.API_URL;
 
 app.use(bodyParser.json()); // Add this line to enable JSON parsing for request bodies
-app.use(morgan('tiny'));
+app.use(morgan('tiny'));  
+
+import { Product } from './models/product.model.js'; 
 
 // console.log('Received a request to the root endpoint.');
-app.get('/api/v1/product*', (req, res) => {
+app.get('/api/v1/product*', async (req, res) => {
     // Your logic here
-    const product = {
-        id: 1,
-        name: 'Phone',
-        image: 'some_URL'
-    };
-    res.send(product);
+    const productList = await Product.find() 
+    if(!productList){
+        res.status(500).json({success:false})
+    }
+    res.send(productList);
 });
 
 
 app.post('/api/v1/product*', (req, res) => {
-    const newProduct = new Product({
+    console.log("Post is working");
+    const product = new Product({
         name : req.body.name,
         image : req.body.image,
         countInStock : req.body.countInStock
     })
 
-    newProduct.save().then((createProduct =>{
+    product.save().then((createProduct =>{
         res.status(201).json(createProduct)
-    })).catch((e)=>{
-        res.status(500).json(
-            {error : e,
-            success : false}
-        )
-    })
+    })).catch((e) => {
+        res.status(500).json({
+            error: e,
+            success: false
+        });
+        return; // Add this line to stop further execution
+    });
+    
 });
 
 app.listen(4000 ,()=>{
