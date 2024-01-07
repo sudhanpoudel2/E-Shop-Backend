3e3e949e03076e122eea9b16571072fc25a42269
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2024-01-07 21:08:57 +0545

    done

--- routers/category.routers.js
+++ routers/category.routers.js
@@ -12,5 +12,38 @@ router.get('/', async (req, res) => {
     res.send(categoryList);
 });
 
+router.post('/',async (req,res)=>{
+    console.log("hello");
+    let category = new Category({
+        name : req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+    });
+     category = await category.save();
+
+    if(!category){
+        return res.status(400).send('the category cannot be created');
+    }
+
+    res.send(category);
+});
+
+router.delete('/:_id', async (req, res) => {
+    console.log("Received delete request for ID:", req.params._id);
+
+    try {
+        const category = await Category.findByIdAndDelete(req.params._id);
+        if (category) {
+            return res.status(200).json({ success: true, message: 'The category is deleted!' });
+        } else {
+            return res.status(404).json({ success: false, message: 'The category is not found' });
+        }
+    } catch (err) {
+        console.error("Error during deletion:", err);
+        return res.status(400).json({ success: false, error: err.message });
+    }
+});
+
+
 export default router;
 
