f2a765fbb384d466d56e0bb050f5275f5b25c536
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2024-01-08 21:53:24 +0545

    id detector using mongoose

--- routers/product.routers.js
+++ routers/product.routers.js
@@ -3,6 +3,7 @@ import { Product } from "../models/product.model.js";
 import { Category } from '../models/category.model.js';
 
 const router = express.Router();
+import mongoose from 'mongoose';
 
 router.get('/', async (req, res) => {
     // yesto garda product ma product name ra image matra show hunchha and id remove hunchha
@@ -47,17 +48,22 @@ router.post('/', async (req, res) => {
         numReviews : req.body.numReviews,
         isFeatured : req.body.isFeatured,
     })
-
     const productSave = await product.save();
 
-    if(!productSave)
-    return res.status(500).send('The product cannot be created')
-
+    if (!productSave) {
+      console.error('Error saving the product:', productSave);
+      return res.status(500).send('The product cannot be created');
+    }
+    
     return res.send(productSave);
     
 });
 
 router.put('/:_id',async (req,res)=>{
+
+    if(!mongoose.isValidObjectId(req.params._id)){
+         return res.status(500).send('Invalid Product Id');
+    }
     const productUpdate = await Product.findByIdAndUpdate(req.params._id,{
         name : req.body.name,
         description : req.body.description,
@@ -76,7 +82,24 @@ router.put('/:_id',async (req,res)=>{
     return res.status(400).send('the product can not be created!');
 
     res.send(productUpdate);
-})
+});
+
+router.delete('/:_id', async (req, res) => {
+    console.log("Received delete request for ID:", req.params._id);
+
+    try {
+        const product = await Product.findByIdAndDelete(req.params._id);
+        if (product) {
+            return res.status(200).json({ success: true, message: 'The product is deleted!' });
+        } else {
+            return res.status(404).json({ success: false, message: 'The product is not found' });
+        }
+    } catch (err) {
+        console.error("Error during deletion:", err);
+        return res.status(400).json({ success: false, error: err.message });
+    }
+});
+
 
 export default router;
 
