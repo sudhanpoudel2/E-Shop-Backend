2953fad7dc3e5977f9c59ffcaf7aab9fe0e20158
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2024-01-18 21:51:01 +0545

    token

--- helper/jwt.js
+++ helper/jwt.js
@@ -1,21 +1,24 @@
-import { expressjwt } from "express-jwt";
-//athentication api protection
+import {expressjwt} from "express-jwt";
+
+// ...
 
 function awthJwt() {
     const secret = process.env.secret;
     return expressjwt({
         secret,
-        algorithms : ['HS512'],
-        isRevoked : isRevoked
+        algorithms: ['HS512'],
+        isRevoked: isRevoked
     }).unless({
-        path:[
-            {url : '/\/api\/v1\/product(.*)/', methods: ['GET','OPTIONS']},
-            {url : '/\/api\/v1\/category(.*)/', methods: ['GET','OPTIONS']},
+        path: [
+            { url: '/\/api\/v1\/product(.*)/', methods: ['GET', 'OPTIONS'] },
+            { url: '/\/api\/v1\/category(.*)/', methods: ['GET', 'OPTIONS'] },
             "/api/v1/user/login",
             "/api/v1/user/register"
         ]
-    })
+    });
 }
+
+// ...
 
 async function isRevoked(req, payload, done){
     if(!payload.isAdmin){
