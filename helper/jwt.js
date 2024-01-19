0ec7dc0949defed78bc9b87c3d1a3e5a7b470303
saransh619 <saranshpachhai@gmail.com>
2024-01-19 22:24:44 +0545

    jwt token authorization fixes

--- helper/jwt.js
+++ helper/jwt.js
@@ -1,9 +1,8 @@
-import {expressjwt} from "express-jwt";
-
-// ...
+import { expressjwt } from "express-jwt";
 
 function awthJwt() {
     const secret = process.env.secret;
+
     return expressjwt({
         secret,
         algorithms: ['HS512'],
@@ -18,10 +17,8 @@ function awthJwt() {
     });
 }
 
-// ...
-
-async function isRevoked(req, payload, done){
-    if(!payload.isAdmin){
+async function isRevoked(req, payload, done) {
+    if (!payload.isAdmin) {
         // Revoke access for non-admin users
         done(null, true);
     } else {
